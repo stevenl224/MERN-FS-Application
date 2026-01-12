b925df2864cecb814ae6a0be3fd0870584ab9b79
Steven Lam <lam.ste@northeastern.edu>
2026-01-12 14:38:08 -0500

    Add get and post workout endpoints

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,34 +1,21 @@
 const express = require('express')
-const Workout = require('../models/workoutModel')
+const {
+    getWorkouts,
+    getWorkout,
+    createWorkout,
+} = require('../controllers/workoutController')
 
 // Create a router
 const router = express.Router()
 
 // GET all workouts
-router.get('/', (req, res) => {
-    res.json({ message: 'GET all workouts '})
-})
+router.get('/', getWorkouts)
 
 // GET single workout
-router.get('/:id', (req, res) => {
-    res.json({ message: 'GET single workout' })
-})
+router.get('/:id', getWorkout)
 
 // POST a new workout
-router.post('/', async (req, res) => {
-    const { title, reps, load } = req.body
-
-    try {
-        const workout = await Workout.create({
-            title,
-            reps,
-            load
-        })
-        res.status(200).json(workout)
-    } catch (error) {
-        res.status(400).json({ error: error.message })
-    }
-})
+router.post('/', createWorkout)
 
 // DELETE a workout
 router.delete('/:id', (req, res) => {
